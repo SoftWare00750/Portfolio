8a70abb76156a0b13ab22885929e22f78802b652
SoftWare00750 <tunesbaby7175@gmail.com>
2026-01-05 08:32:13 +0100

    COMMIT15

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -2,7 +2,7 @@
 const projects = [
       
                                   {
-                                      id: "proj-2",
+                                      id: "proj-1",
                                           title: "Nike Shoe store website",
                                               desc: "View different Nike shoes and purchase them.",
                                                   tech: ["React.js", "Django", "SQLite"],
@@ -11,7 +11,7 @@ const projects = [
                                                             },
 
                                                             {
-                                      id: "proj-3",
+                                      id: "proj-2",
                                           title: "E-commerce store",
                                               desc: "Check out different items and purchase them.",
                                                   tech: ["React.js", "PHP", "PostgreSQL"],
@@ -20,14 +20,25 @@ const projects = [
                                                             },
 
                                                                                  {
-                                      id: "proj-4",
+                                      id: "proj-3",
                                           title: "Maglo Finance Web Application ",
                                               desc: "Manage your finances, Monitor your profits and Create invoices for items .",
                                                   tech: [ "Next.js", "Express.js", "PostgreSQL"],
                                                    image: "../../assets/maglo.png",
                                                           link: "https://maglo-three.vercel.app/"
-                                                            }
+                                                            },
+
+                                                             {
+    id: "proj-4",
+    title: "Task Management App, Taskflow",
+    desc: "Manage your Tasks, per day, per week, per month, per year.",
+    tech: ["Vue.js","Django", "MySQL"],
+    image: "/assets/taskflow.png",
+    link: "https://taskflow-frontend-dun.vercel.app/"
+    
+  }
                                     
+                                                            
                                                             ];
 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          export default projects;
